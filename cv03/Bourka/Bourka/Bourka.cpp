fbfe02245056ef89d06360b26efc500ea13a3507
unknown <Student@PC-SE5115-09.utko.feec.vutbr.cz>
2025-10-02 12:38:17 +0200

    2.10.2025 treti cviko

---
+++ cv03/Bourka/Bourka/Bourka.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include "Bourka.h"
+
+using namespace std;
+
+int main()
+{
+	int cas;
+	printf("zadej cas v sekundach:");
+	scanf("%d",&cas);
+	int vysledek = cas * 340;
+	printf("\nvzdalenost mista kam hrom uhodil: %d metru\n", vysledek);
+	
+	return 0;
+}
---
+++ cv03/komplexnicislo/komplexnicislo/komplexnicislo.cpp
@@ -0,0 +1,29 @@
+﻿// komplexnicislo.cpp : Defines the entry point for the application.
+//
+
+#include "komplexnicislo.h"
+
+using namespace std;
+
+int main()
+{
+	int realA;
+	int imgA;
+	int realB;
+	int imgB;
+
+		printf("Zadej realnou cast cisla A: ");
+	scanf("%d",&realA);
+		printf("Zadej imaginarni cast cisla A: ");
+	scanf("%d",&imgA);
+		printf("Zadej realnou cast cisla B: ");
+	scanf("%d",&realB);
+		printf("Zadej imaginarni cast cisla B: ");
+	scanf("%d",&imgB);
+
+	int soucetreal = realA + realB;
+	int soucetimg = imgA + imgB;
+
+	printf("\nsoucet cisla A a cisla B je %d + %di\n\n\n", soucetreal, soucetimg);
+	return 0;
+}
